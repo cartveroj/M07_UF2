7ba3b05702dea1ff9a02858bcc96cc6bad314242
veronica <cartagenaveronica.19@gmail.com>
2023-12-19 17:26:32 +0100

    resolviendo conflictos

--- my-project/routes/admin.php
+++ my-project/routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AlumnatController;
+use App\Http\Controllers\Admin\CentresController;
 use App\Http\Controllers\Admin\ProfessoratController;
 
     // Ambits de rutes de admin_db
@@ -19,13 +21,31 @@ use App\Http\Controllers\Admin\ProfessoratController;
 
         Route::get('/admin_view', [AdminController::class, 'adminView'])->name('admin_view'); // retorna a la vista de admin
         
+        //Rutas Alumnat CRUD
+        Route::get('/getAlumnat', [AlumnatController::class, 'index'])->name('getAlumnat');//muestran todos los alumnos de la bbdd
+        Route::get('/formAlumnat', [AlumnatController::class, 'create'])->name('createAlumnat');//es el form
+        Route::post('/insertAlumnat', [AlumnatController::class, 'store'])->name('insertAlumnat');//gestiona formulario
+        Route::get('/alumnat/{id}/edit',  [AlumnatController::class, 'edit'])->name('editAlumnat');//es el form
+        Route::put('/updateAlumnat/{id}', [AlumnatController::class, 'update'])->name('updateAlumnat');//gestiona el formulario
+        Route::delete('/deleteAlumnat/{id}', [AlumnatController::class, 'destroy'])->name('destroyAlumnat');
+        Route::get('/alumnat/{id}', [AlumnatController::class, 'show'])->name('showAlumnat'); //muestra 1 alumno por id
 
+
+        /*  Rutas CRUD de Centres */
+        Route::get('/getCentres', [CentresController::class, 'index'])->name('getCentres');  // obté tots els centres
+        Route::get('/formCentres', [CentresController::class, 'create'])->name('formCentres'); // mostra el formulari de registre   
+        Route::post('/insertCentres', [CentresController::class, 'store'])->name('insertCentres'); // ruta de registre
+        Route::get('/centres/{id}', [CentresController::class, 'show'])->name('showCentre'); // mostra un centre per id
+        Route::get('/centres/{id}/edit', [CentresController::class, 'edit'])->name('editCentres'); // mostra el formulari de edició
+        Route::put('/updateCentres/{id}', [CentresController::class, 'update'])->name('updateCentres'); // actualitza centre
+        Route::delete('/deleteCentres/{id}', [CentresController::class, 'destroy'])->name('deleteCentres'); // elimina centre
+        
         //professorat
-         Route::get('/GetProfessorat', [ProfessoratController::class, 'index'])->name('getProfessorat');
-         Route::get('/formProfessorat', [ProfessoratController::class, 'create'])->name('createProfessorat');
-         Route::post('/InsertProfessorat', [ProfessoratController::class, 'store'])->name('insertProfessorat');
-         Route::get('/professorat/{id}/edit', [ProfessoratController::class, 'edit'])->name('editProfessorat');
-         Route::put('/UpdateProfessorat/{id}', [ProfessoratController::class, 'update'])->name('updateProfessorat');
-         Route::delete('/DeleteProfessorat/{id}', [ProfessoratController::class, 'destroy'])->name('destroyProfessorat');
-         Route::get('/professorat/{id}', [ProfessoratController::class, 'show'])->name('showProfessorat');
+         Route::get('/GetProfessorat', [ProfessoratController::class, 'index'])->name('getProfessorat'); //retorna todos los profesores de bbdd
+         Route::get('/formProfessorat', [ProfessoratController::class, 'create'])->name('createProfessorat'); // muestra el form de registro
+         Route::post('/InsertProfessorat', [ProfessoratController::class, 'store'])->name('insertProfessorat'); // hace el insert a la bbdd
+         Route::get('/professorat/{id}/edit', [ProfessoratController::class, 'edit'])->name('editProfessorat'); //muestra el form para editar un registro
+         Route::put('/UpdateProfessorat/{id}', [ProfessoratController::class, 'update'])->name('updateProfessorat'); // realiza el update a la bbdd
+         Route::delete('/DeleteProfessorat/{id}', [ProfessoratController::class, 'destroy'])->name('destroyProfessorat'); // elimina el registro de la bbdd
+         Route::get('/professorat/{id}', [ProfessoratController::class, 'show'])->name('showProfessorat'); //muestra el registro seleccionado
         }); 
